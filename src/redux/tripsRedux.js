6e550f13396524d457ca92990b598708cef8b503
Sebastian Gubiec <seb.gub@wp.pl>
2021-11-16 23:42:06 +0100

    finish_17.5

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,16 +12,20 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
-
+  if (filters.duration) {
+    output = output.filter(trip => (trip.days <= filters.duration.to && trip.days >= filters.duration.from));
+  }
   // TODO - filter by tags
-
+  if (filters.tags) {
+    output = output.filter(trip => filters.tags.every(tag => trip.tags.includes(tag)));
+  }
   // TODO - sort by cost descending (most expensive goes first)
 
   return output;
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
+  const filtered = trips.filter(trip => trip.tripId === tripId);
 
   // TODO - filter trips by tripId
 
@@ -30,7 +34,7 @@ export const getTripById = ({trips}, tripId) => {
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+  const filtered = trips.filter(trip => trip.countryCode === countryCode);
 
   // TODO - filter trips by countryCode
 
