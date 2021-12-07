7f69ff38f8030bf6a571e6f3300590bb57aa66e0
Sebastian Gubiec <seb.gub@wp.pl>
2021-12-07 22:35:51 +0100

    sendOrder_to_API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -6,8 +6,46 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import Section from '../../layout/Section/Section';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import settings from '../../../data/settings';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import Button from '../../common/Button/Button';
 
-const OrderForm = ({ options, tripCost, setOrderOption }) => {
+const sendOrder = (options, tripCost, tripId, tripName, countryCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName,
+    tripId,
+    countryCode,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  if (payload.name === '' || payload.contact === '') {
+    alert('Please complete the name and contact fields');
+  }
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({ options, tripCost, setOrderOption, tripName, countryCode, tripId }) => {
   console.log('tripCost', tripCost);
   return (
     <Section>
@@ -20,6 +58,7 @@ const OrderForm = ({ options, tripCost, setOrderOption }) => {
           ))}
           <Col xs={12}>
             <OrderSummary options={options} tripCost={tripCost} />
+            <Button onClick={() => sendOrder(options, tripCost, tripId, tripName, countryCode)}>Order now!</Button>
           </Col>
         </Row>
       </Grid>
@@ -29,8 +68,8 @@ const OrderForm = ({ options, tripCost, setOrderOption }) => {
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
-  options: PropTypes.node,
-  setOrderOption: PropTypes.node,
+  options: PropTypes.object,
+  setOrderOption: PropTypes.func,
   tripName: PropTypes.string,
   countryCode: PropTypes.string,
   tripId: PropTypes.string,
