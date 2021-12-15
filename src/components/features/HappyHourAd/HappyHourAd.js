69bf9614d7fd8735c7918328b8ee1ca1fe119467
Sebastian Gubiec <seb.gub@wp.pl>
2021-12-16 00:55:57 +0100

    happyHours_component

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './HappyHourAd.module.scss';
+import PropTypes from 'prop-types';
+
+class HappyHourAd extends React.Component {
+  getCountdownTime(){
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+  
+    if(currentTime.getUTCHours() >= 12){
+      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    }
+  
+    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+  }
+
+  constructor(){
+    super();
+    setInterval(()=> this.forceUpdate(), 1000);
+  }
+
+  render() {
+    let {promoDescription, title} = this.props;
+    const seconds = this.getCountdownTime();
+
+    return(
+      <div className={styles.component}>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.promoDescription}>{seconds > 23*60*60 ? promoDescription: seconds}</div>
+      </div>
+    );
+  }
+}
+
+HappyHourAd.propTypes = {
+  promoDescription: PropTypes.string,
+  title: PropTypes.string,
+};
+
+export default HappyHourAd;
