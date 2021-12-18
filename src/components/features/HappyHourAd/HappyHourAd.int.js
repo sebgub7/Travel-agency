059b2ec4104b20ea23be8dcaaaefa5bdf713fd5c
Sebastian Gubiec <seb.gub@wp.pl>
2021-12-18 14:30:28 +0100

    formatTime

--- src/components/features/HappyHourAd/HappyHourAd.int.js
+++ src/components/features/HappyHourAd/HappyHourAd.int.js
@@ -12,30 +12,6 @@ const mockProps = {
   promoDescription: 'promoDescriptionMockProps',
 };
 
-beforeAll(() => {
-  const utilsModule = jest.requireActual('../../../utils/formatTime.js');
-  utilsModule.formatTime = jest.fn(seconds => seconds);
-});
-
-describe('Component HappyHourAd', () => {
-  it('should render without crashing', () => {
-    const component = shallow(<HappyHourAd />);
-    expect(component).toBeTruthy();
-    console.log(component.debug());
-  });
-
-  it('should render title and  description', () => {
-    const component = shallow(<HappyHourAd />);
-    expect(component.exists(select.title)).toEqual(true);
-    expect(component.exists(select.promoDescription)).toEqual(true);
-  });
-
-  it('should render title', () => {
-    const component = shallow(<HappyHourAd {...mockProps} />);
-    expect(component.find(select.title).text()).toBe(mockProps.title);
-  });
-});
-
 const trueDate = Date;
 const mockDate = (customDate) => class extends Date {
   constructor(...args) {
@@ -69,18 +45,17 @@ describe('Component HappyHourAd with mocked Date', () => {
   checkDescriptionAtTime('13:00:00', 23 * 60 * 60 + '');
 });
 
-const checkDescriptionAfterTime = (time, delaySeconds, expectedDescription) => {
-  it(`should show correct value ${delaySeconds} seconds after ${time}`, () => {
+const checkDescriptionAfterTime = (time, delaySecounds, expectedDescription) => {
+  it(`should show correct value ${delaySecounds} seconds after ${time}`, () => {
     jest.useFakeTimers();
     global.Date = mockDate(`2019-05-14T${time}.135Z`);
 
     const component = shallow(<HappyHourAd {...mockProps} />);
     const newTime = new Date();
-    newTime.setSeconds(newTime.getSeconds() + delaySeconds);
+    newTime.setSeconds(newTime.getSeconds() + delaySecounds);
     global.Date = mockDate(newTime.getTime());
 
-    jest.advanceTimersByTime(delaySeconds * 1000);
-
+    jest.advanceTimersByTime(delaySecounds * 1000);
     const renderedTime = component.find(select.promoDescription).text();
     expect(renderedTime).toEqual(expectedDescription);
 
@@ -93,10 +68,4 @@ describe('Component HappyHourAd with mocked Date and delay', () => {
   checkDescriptionAfterTime('11:57:58', 2, '120');
   checkDescriptionAfterTime('11:59:58', 1, '1');
   checkDescriptionAfterTime('13:00:00', 60 * 60, 22 * 60 * 60 + '');
-});
-
-describe('Component HappyHourAd - visible time 12:00:00 - 12:59:59', () => {
-  checkDescriptionAtTime('12:00:00', mockProps.promoDescription);
-  checkDescriptionAtTime('12:30:00', mockProps.promoDescription);
-  checkDescriptionAtTime('12:59:59', mockProps.promoDescription);
 });
--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import styles from './HappyHourAd.module.scss';
 import PropTypes from 'prop-types';
+import { formatTime } from '../../../utils/formatTime';
 
 class HappyHourAd extends React.Component {
-  getCountdownTime(){
+  getCountdownTime() {
     const currentTime = new Date();
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
-  
-    if(currentTime.getUTCHours() >= 12){
-      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
     }
-  
-    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
   }
 
-  constructor(){
+  constructor() {
     super();
-    setInterval(()=> this.forceUpdate(), 1000);
+    setInterval(() => this.forceUpdate(), 1000);
   }
 
   render() {
-    let {promoDescription, title} = this.props;
-    const seconds = this.getCountdownTime();
+    let { promoDescription, title } = this.props;
+    const time = this.getCountdownTime();
 
-    return(
+    return (
       <div className={styles.component}>
         <h3 className={styles.title}>{title}</h3>
-        <div className={styles.promoDescription}>{seconds > 23*60*60 ? promoDescription: seconds}</div>
+        <div className={styles.promoDescription}>{time > 23 * 60 * 60 ? promoDescription : formatTime(time)}</div>
       </div>
     );
   }
